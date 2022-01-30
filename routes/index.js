f91438f26fa541b6dcdf7d3153ded0fd65fb300c
Harold Diaz <harold.vargas@bewe.io>
2022-01-30 16:05:12 -0500

    integrando node y mongo con docker

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // {description: 'going to the gym', complete: true}
+  res.render('index', { title: 'Express', todos: [] });
 });
 
 module.exports = router;
