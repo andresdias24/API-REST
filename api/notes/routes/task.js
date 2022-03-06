19b401810b40bc882cee3db7adfeb22dda739235
Harold Diaz <harold.vargas@bewe.io>
2022-03-05 23:05:26 -0500

    agragando documentos a mongo

--- api/notes/routes/task.js
+++ api/notes/routes/task.js
@@ -3,16 +3,17 @@ var router = express.Router();
 var {Task}  = require('../../../common/models/Task')
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-
+  const tasks = await Task.find().lean()
   // {description: 'going to the gym', complete: true}
-  // res.render('index', { title: 'Todos', todos: todos });
-  res.render('index');
+  res.render('index', { tasks: tasks });
 });
 
 router.post('/tasks/add', async (req, res) => {
   const task = Task(req.body)
   const taskSave = await task.save()
-  res.redirect("/task")
+  console.log("😆👽🕳👨‍💻 🧬 ~ file: task.js ~ line 15 ~ taskSave", taskSave)
+  // res.redirect("/task")
+  res.status(200)
 })
 
 module.exports = router;
